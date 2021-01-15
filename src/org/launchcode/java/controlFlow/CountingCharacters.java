3ce4b7794876762cc81f5f016282b97eb3490b0c
romeo adjam <sodangbe@users.noreply.github.com>
2021-01-14 19:47:47 -0600

    Adding studio counting character studio

---
+++ src/org/launchcode/java/controlFlow/CountingCharacters.java
@@ -0,0 +1,40 @@
+package org.launchcode.java.controlFlow;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountingCharacters {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //String text = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        //Scanner input = new Scanner(System.in);
+        //System.out.println("Enter your test:");
+        String textRaw = new String();
+        File file = new File("C:\\test.txt");
+        Scanner scannedFile = new Scanner(file);
+
+        while (scannedFile.hasNextLine()){
+             textRaw = scannedFile.nextLine();
+        }
+
+        String text = textRaw.toLowerCase();
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+
+        for (int i=0; i<text.length()-1; i++){
+            if (hashMap.containsKey(text.charAt(i))) {
+                int count = hashMap.get(text.charAt(i));
+                hashMap.put(text.charAt(i), ++count);
+            } else {
+                hashMap.put(text.charAt(i),1);
+            }
+        }
+        //System.out.println("Counting occurrences of each character = "+hashMap);
+        for(Map.Entry<Character, Integer> character: hashMap.entrySet()){
+            System.out.println(character.getValue() + ":" + character.getKey());
+        }
+    }
+}
--- src/org/launchcode/java/controlFlow/HashMapPractice.java
+++ src/org/launchcode/java/controlFlow/HashMapPractice.java
@@ -1,8 +1,44 @@
 package org.launchcode.java.controlFlow;
 
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class HashMapPractice {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashMap<Integer,String> roster = new HashMap<>();
 
+        String newStudent;
+
+        System.out.println("Student and ID : ");
+        do {
+
+            System.out.print("Student: ");
+            newStudent = input.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.print("ID: ");
+                Integer newID = input.nextInt();
+                roster.put(newID,newStudent);
+
+                // Read in the newline before looping back
+                input.nextLine();
+            }
+
+        } while(!newStudent.equals(""));
+
+        input.close();
+        System.out.println("Roster");
+
+        for(Map.Entry<Integer, String> student: roster.entrySet()){
+            System.out.println(student.getValue() + "'s ID:" + student.getKey());
+        }
+
+        int size = roster.size();
+        String result = String.format("Number of students :%s", size);
+        System.out.println(result);
     }
 }
