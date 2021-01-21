6e34715cdbb5faed0a324bfa768f906b84b6349c
romeo adjam <sodangbe@users.noreply.github.com>
2021-01-21 17:04:22 -0600

    Added course and teacher class

--- src/org/launchcode/java/demos/lsn3classes1/Teacher.java
+++ src/org/launchcode/java/demos/lsn3classes1/Teacher.java
@@ -1,4 +1,25 @@
 package org.launchcode.java.demos.lsn3classes1;
 
 public class Teacher {
+
+    private String firstName;
+    private String lastName;
+    private String subject;
+    private int yearsTeaching;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getYearsTeaching() {
+        return yearsTeaching;
+    }
 }
