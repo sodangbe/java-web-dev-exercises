73788751c91a199e01ce104e7c8725fd9cf43596
romeo adjam <sodangbe@users.noreply.github.com>
2021-01-21 16:47:03 -0600

    Added course and teacher class

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,13 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student mySelf = new Student();
+        mySelf.setName("Romeo");
+        mySelf.setNumberOfCredits(1);
+        mySelf.setGpa(4.0);
+
+      mySelf.printStudent();
+
+
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -3,6 +3,8 @@ package org.launchcode.java.demos.lsn3classes1;
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Student {
 
     private String name;
@@ -10,4 +12,30 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    void printStudent(){
+        String stud = String.format("Name : %s \nCredits : %s \nGPA : %s",this.name,this.numberOfCredits,this.gpa);
+        System.out.println(stud);
+    }
+    //    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+//        this.name = name;
+//        this.studentId = studentId;
+//        this.numberOfCredits = numberOfCredits;
+//        this.gpa = gpa;
+//    }
 }
